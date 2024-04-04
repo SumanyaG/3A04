30080baa6e2b09705c8751338863bc7833360371
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-04-04 00:14:17 -0400

    iterates through db groups

--- D4/SecureChat/app/src/main/java/com/example/securechat/Fragments/GroupsFragment.java
+++ D4/SecureChat/app/src/main/java/com/example/securechat/Fragments/GroupsFragment.java
@@ -2,29 +2,91 @@ package com.example.securechat.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.securechat.Controllers.ChatController;
 import com.example.securechat.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.cert.PolicyNode;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class GroupsFragment extends Fragment {
+
+    private View groupFragmentView;
+    private ListView list_view;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_groups = new ArrayList<>();
+
+    private DatabaseReference chatDb;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_groups, container, false);
+        View groupFragmentView = inflater.inflate(R.layout.fragment_groups, container, false);
 
-        Button createGroupButton = view.findViewById(R.id.createGroupButton);
+        chatDb = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+        InitializeFields(groupFragmentView);
+
+        RetrieveAndDisplayGroups();
+
+        Button createGroupButton = groupFragmentView.findViewById(R.id.createGroupButton);
         createGroupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((ChatController) requireActivity()).RequestNewGroup();
             }
         });
-        return view;
+
+        return groupFragmentView;
     }
+
+
+    private void InitializeFields(View groupFragmentView) {
+        list_view = (ListView) groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_groups);
+        list_view.setAdapter(arrayAdapter);
+    }
+
+    private void RetrieveAndDisplayGroups() {
+        chatDb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<>();
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext()) {
+                    set.add(((DataSnapshot) iterator.next()).getKey());
+                }
+
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
 }
